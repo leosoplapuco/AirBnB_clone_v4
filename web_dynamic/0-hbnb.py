dc8b6da044e666e19381334ffd78c9c52270d632
leosoplapuco <leosoplapuco29@gmail.com>
2024-03-08 08:42:54 -0500

    Remove changes on 0-hbnb

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -1,29 +1,45 @@
 #!/usr/bin/python3
-"""
-starts a Flask web application
-"""
-
-from flask import Flask, render_template
-from models import *
+""" Starts a Flash Web Application """
 from models import storage
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from os import environ
+from flask import Flask, render_template
 app = Flask(__name__)
-
-
-@app.route('/0-hbnb', strict_slashes=False)
-def filters():
-    """display a HTML page like 6-index.html from static"""
-    states = storage.all("State").values()
-    amenities = storage.all("Amenity").values()
-    return render_template('0-hbnb.html', states=states,
-                           amenities=amenities)
+# app.jinja_env.trim_blocks = True
+# app.jinja_env.lstrip_blocks = True
 
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    """   closes the storage on teardown   """
+def close_db(error):
+    """ Remove the current SQLAlchemy Session """
     storage.close()
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
 
-#To run app -> python3 -m web_dynamic.0-hbnb
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ HBNB is alive! """
+    states = storage.all(State).values()
+    states = sorted(states, key=lambda k: k.name)
+    st_ct = []
+
+    for state in states:
+        st_ct.append([state, sorted(state.cities, key=lambda k: k.name)])
+
+    amenities = storage.all(Amenity).values()
+    amenities = sorted(amenities, key=lambda k: k.name)
+
+    places = storage.all(Place).values()
+    places = sorted(places, key=lambda k: k.name)
+
+    return render_template('100-hbnb.html',
+                           states=st_ct,
+                           amenities=amenities,
+                           places=places)
+
+
+if __name__ == "__main__":
+    """ Main Function """
+    app.run(host='0.0.0.0', port=5000)
